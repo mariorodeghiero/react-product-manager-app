1568464c8718014006023b3e2820c5a9018c620e
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-06-03 22:16:44 -0300

    configured bootstrap and created navbar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,39 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <button className="btn btn-primary">Test</button>
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link active" href="#">
+                Products Manager<span class="sr-only">(current)</span>
+              </a>
+              <a class="nav-item nav-link" href="#">
+                Home
+              </a>
+              <a class="nav-item nav-link" href="#">
+                Products
+              </a>
+              <a class="nav-item nav-link disabled" href="#">
+                About
+              </a>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
